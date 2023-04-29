595008116b2e52348ef0cd1f1c1753d425bfdb45
BlackDilvish <gabriel.naleznik@gmail.com>
2023-04-29 11:35:21 +0200

    Fix nested objects slice

--- Assets/Scripts/Slicer.cs
+++ Assets/Scripts/Slicer.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using UnityEngine;
 using EzySlice;
 
@@ -11,6 +13,8 @@ public class Slicer : MonoBehaviour
 
     [SerializeField] private LayerMask sliceableLayer;
 
+    private IList<GameObject> slicedFragments_ = new List<GameObject>();
+
     void FixedUpdate()
     {
         bool isHit = Physics.Linecast(beginSlicePoint.position, endSlicePoint.position, out RaycastHit hit, sliceableLayer);
@@ -21,7 +25,17 @@ public class Slicer : MonoBehaviour
         }
     }
 
-    public void Slice(GameObject target)
+    public void ClearSlicedFragments()
+    {
+        foreach (var obj in this.slicedFragments_)
+        {
+            Destroy(obj);
+        }
+
+        this.slicedFragments_.Clear();
+    }
+
+    private void Slice(GameObject target)
     {
         Vector3 velocity = velocityEstimator.GetVelocityEstimate();
         Vector3 planeNormal = Vector3.Cross(endSlicePoint.position - beginSlicePoint.position, velocity);
@@ -32,19 +46,26 @@ public class Slicer : MonoBehaviour
         if (hull != null)
         {
             GameObject upperHull = hull.CreateUpperHull(target, sliceTraceMaterial);
-            this.SetupSlicedObject(upperHull);
+            this.SetupSlicedObject(upperHull, target.transform.parent);
             GameObject lowerHull = hull.CreateLowerHull(target, sliceTraceMaterial);
-            this.SetupSlicedObject(lowerHull);
+            this.SetupSlicedObject(lowerHull, target.transform.parent);
 
             Destroy(target);
         }
     }
 
-    private void SetupSlicedObject(GameObject obj)
+    private void SetupSlicedObject(GameObject obj, Transform parentOffset = null)
     {
         Rigidbody rigidbody = obj.AddComponent<Rigidbody>();
         MeshCollider collider = obj.AddComponent<MeshCollider>();
         collider.convex = true;
         obj.layer = Mathf.RoundToInt(Mathf.Log(sliceableLayer.value, 2));
-    }
+
+        if (parentOffset != null)
+        {
+            obj.transform.position += parentOffset.position;
+        }
+
+        slicedFragments_.Add(obj);
+    }   
 }
